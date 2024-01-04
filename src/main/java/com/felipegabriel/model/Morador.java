756cd34b3ea720d19daef153a646dae4dba8fc5c
Felipe Gabriel Gervazio Sotero <felipegabriel@sistran.com.br>
2024-01-04 15:29:48 -0300

    Criação model

--- src/main/java/com/felipegabriel/model/Morador.java
+++ src/main/java/com/felipegabriel/model/Morador.java
@@ -1,5 +1,56 @@
 package com.felipegabriel.model;
 
-public class Morador {
+public class Morador extends BaseEntity {
+	private String nome;
+	private String numero;
+	private String cpf;
+	private String numGaragem;
+	private String bloco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNumGaragem() {
+		return numGaragem;
+	}
+
+	public void setNumGaragem(String numGaragem) {
+		this.numGaragem = numGaragem;
+	}
+
+	public String getBloco() {
+		return bloco;
+	}
+
+	public void setBloco(String bloco) {
+		this.bloco = bloco;
+	}
+
+	@Override
+	public String toString() {
+		return "Morador [nome=" + nome + ", numero=" + numero + ", cpf=" + cpf + ", numGaragem=" + numGaragem
+				+ ", bloco=" + bloco + "]";
+	}
 
 }
--- src/main/java/com/felipegabriel/model/Veiculo.java
+++ src/main/java/com/felipegabriel/model/Veiculo.java
@@ -1,5 +1,38 @@
 package com.felipegabriel.model;
 
-public class Veiculo {
+public class Veiculo extends BaseEntity {
+	private String cor;
+	private String placa;
+	private String marca;
 
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public String toString() {
+		return "Veiculo [cor=" + cor + ", placa=" + placa + ", marca=" + marca + "]";
+	}
+
+	
 }
